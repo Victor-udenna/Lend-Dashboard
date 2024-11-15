a496f744f1542a788809447433164e79a7b02aed
Victor udenna <victorudenna@gmail.com>
2024-11-15 08:36:02 +0100

    feat(user) user table empty state

--- src/Components/organism/UserTable/UserTable.tsx
+++ src/Components/organism/UserTable/UserTable.tsx
@@ -100,6 +100,131 @@ export default function UserTable({ data }: UserTableProps) {
     window.location.reload();
   };
 
+  // return (
+
+  //   <div className="user-table__container" ref={tableRef}>
+  //     {filter && (
+  //       <div ref={filterRef}>
+  //         <TableFilter onFilterApply={applyFilter} />
+  //       </div>
+  //     )}
+  //     <table className="user-table">
+  //       <thead>
+  //         <tr>
+  //           <th>
+  //             <div className="user-table__header">
+  //               <Text>ORGANIZATION</Text>
+  //               <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+  //             </div>
+  //           </th>
+  //           <th>
+  //             <div className="user-table__header">
+  //               <Text>USERNAME</Text>
+  //               <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+  //             </div>
+  //           </th>
+  //           <th className="email_header">
+  //             <div className="user-table__header">
+  //               <Text>EMAIL</Text>
+  //               <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+  //             </div>
+  //           </th>
+  //           <th className="phone_header">
+  //             <div className="user-table__header">
+  //               <Text>PHONE NUMBER</Text>
+  //               <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+  //             </div>
+  //           </th>
+  //           <th className="date_joined">
+  //             <div className="user-table__header">
+  //               <Text>DATE JOINED</Text>
+  //               <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+  //             </div>
+  //           </th>
+  //           <th>
+  //             <div className="user-table__header status_header">
+  //               <Text>STATUS</Text>
+  //               <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+  //             </div>
+  //           </th>
+  //           <th className="user-table__moreaction">
+  //             <Text>{'More'}</Text>
+  //           </th>
+  //         </tr>
+  //       </thead>
+  //       {filteredData.length > 0 ? (
+  //         <tbody>
+  //           {filteredData.map((user: User) => (
+  //             <tr key={user.userid} className="user-table__row">
+  //               <td className="user-table__organization">{'Lendsqr'}</td>
+  //               <td
+  //                 onClick={() => router.push(`/dashboard/users/details?userid=${user.userid}`)}
+  //                 className="user-table__username"
+  //               >
+  //                 {user.personal_information.firstname + ' ' + user.personal_information.lastname}
+  //               </td>
+  //               <td className="user-table__email">{user.personal_information.email.substring(0, 15)}</td>
+  //               <td className="user-table__phone">{user.personal_information.phone_number.substring(0, 10)}</td>
+  //               <td className="user-table__date-joined">{new Date(user.dateJoined).toLocaleDateString()}</td>
+  //               <td className="user-table__status">
+  //                 <Text className={`user-table__status-text ${user.status}`}>{user.status}</Text>
+  //               </td>
+  //               <td>
+  //                 <Button
+  //                   className="user-table__actionbtn"
+  //                   imageAlt="more action button"
+  //                   imageHeight={14.44}
+  //                   imageWidth={3.33}
+  //                   imageSrc={moreactionImg}
+  //                   onClick={() => handlePopup(user.userid)}
+  //                 />
+  //                 {selectedId === user.userid && isPopup && (
+  //                   <div ref={popupRef} className="user-table__menu">
+  //                     <Button
+  //                       position="left"
+  //                       imageHeight={9.73}
+  //                       imageWidth={15.1}
+  //                       imageSrc={viewIcon}
+  //                       textValue="View Details"
+  //                       onClick={() => router.push(`/dashboard/users/details?userid=${user.userid}`)}
+  //                     />
+  //                     {user.status === 'active' ? (
+  //                       <Button
+  //                         position="left"
+  //                         imageHeight={14}
+  //                         imageWidth={14}
+  //                         imageSrc={blackListuser}
+  //                         textValue="Blacklist User"
+  //                         onClick={() => {
+  //                           handleStatusChange(user?.id, 'blacklisted');
+  //                         }}
+  //                       />
+  //                     ) : (
+  //                       <Button
+  //                         position="left"
+  //                         imageHeight={14}
+  //                         imageWidth={14}
+  //                         imageSrc={activeUserIcon}
+  //                         textValue="Activate User"
+  //                         onClick={() => {
+  //                           handleStatusChange(user?.id, 'active');
+  //                         }}
+  //                       />
+  //                     )}
+  //                   </div>
+  //                 )}
+  //               </td>
+  //             </tr>
+  //           ))}
+  //         </tbody>
+  //       ) : (
+  //         <div className="user-table__empty-state">
+  //           <EmptyState title="No data Found" desc="try refreshing this page to view data" />
+  //         </div>
+  //       )}
+  //     </table>
+  //   </div>
+  // );
   return (
     <div className="user-table__container" ref={tableRef}>
       {filter && (
@@ -107,7 +232,6 @@ export default function UserTable({ data }: UserTableProps) {
           <TableFilter onFilterApply={applyFilter} />
         </div>
       )}
-      {/* {filteredData.length > 0 ? ( */}
       <table className="user-table">
         <thead>
           <tr>
@@ -131,6 +255,7 @@ export default function UserTable({ data }: UserTableProps) {
             </th>
             <th className="phone_header">
               <div className="user-table__header">
+                {' '}
                 <Text>PHONE NUMBER</Text>
                 <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
               </div>
@@ -138,7 +263,7 @@ export default function UserTable({ data }: UserTableProps) {
             <th className="date_joined">
               <div className="user-table__header">
                 <Text>DATE JOINED</Text>
-                <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />
+                <ImageAtom onClick={toggleFilter} alt="filter-icon" width={16} height={16} src={filterIcon} />{' '}
               </div>
             </th>
             <th>
@@ -156,7 +281,7 @@ export default function UserTable({ data }: UserTableProps) {
           <tbody>
             {filteredData.map((user: User) => (
               <tr key={user.userid} className="user-table__row">
-                <td className="user-table__organization">{'Lendsqr'}</td>
+                <td className="user-table__organization">Lendsqr</td>
                 <td
                   onClick={() => router.push(`/dashboard/users/details?userid=${user.userid}`)}
                   className="user-table__username"
@@ -218,9 +343,13 @@ export default function UserTable({ data }: UserTableProps) {
             ))}
           </tbody>
         ) : (
-          <div className="user-table__empty-state">
-            <EmptyState title="No data Found" desc="try refreshing this page to view data" />
-          </div>
+          <tbody>
+            <tr>
+              <td colSpan={7}>
+                <EmptyState title="No data Found" desc="Try refreshing this page to view data" />
+              </td>
+            </tr>
+          </tbody>
         )}
       </table>
     </div>
